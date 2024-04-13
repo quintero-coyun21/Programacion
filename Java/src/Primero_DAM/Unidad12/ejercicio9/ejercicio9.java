35c30cf4a3065eae3743e5fe64d36186f8e4d35d
quintero-coyun21 <118376512+quintero-coyun21@users.noreply.github.com>
2024-04-13 15:48:39 +0200

    Unidad 12
    Ejercicio 9 terminado

--- Java/src/Primero_DAM/Unidad12/ejercicio9/ejercicio9.java
+++ Java/src/Primero_DAM/Unidad12/ejercicio9/ejercicio9.java
@@ -1,19 +1,40 @@
 package Primero_DAM.Unidad12.ejercicio9;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ejercicio9 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Parte 1
         ArrayList<Integer> integerArrayList = new ArrayList<>();
-        int numero = 0;
-        while (numero > 0){
-            System.out.println("Introduce un número entero positivo: ");
-            numero = scanner.nextInt();
-            for (var indice : integerArrayList) {
-                integerArrayList.add(numero);
+        Scanner scanner = new Scanner(System.in);
+        Integer numero;
+
+        boolean entradaValida = false;
+        while (!entradaValida) {
+            try {
+                System.out.println("Introduce un número entero positivo (para salir introduce un entero negativo): ");
+                numero = scanner.nextInt();
+                if (numero >= 0) {
+                    integerArrayList.add(numero);
+                } else {
+                    entradaValida = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes ingresar un número entero.");
+                scanner.next();
             }
         }
+
+        //Parte 2
+        int contador = 0;
+        for (var entero : integerArrayList) {
+            if (entero % 2 == 0){
+                System.out.println("El índice de " + entero + " es " + contador + " x100 es: " + entero*100);
+            }
+            contador++;
+        }
     }
+
 }
