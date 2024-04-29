4a1d7d18f018c4578d5d41f889e502e662f201b0
quintero-coyun21 <118376512+quintero-coyun21@users.noreply.github.com>
2024-04-29 18:29:14 +0200

    Práctica 5
    seguir

---
+++ Java/src/Primero_DAM/Practica5/Ejercicio1.java
@@ -0,0 +1,7 @@
+package Primero_DAM.Practica5;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java/src/Primero_DAM/Practica5/OperacionesBasicas.java
@@ -0,0 +1,8 @@
+package Primero_DAM.Practica5;
+
+public interface OperacionesBasicas {
+    public void sumaElementos();
+    public void promedioElementos();
+    public void maxElemento();
+    public void minElemento();
+}
