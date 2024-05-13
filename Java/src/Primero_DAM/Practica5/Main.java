9d323dedaa7f49819ca6093caaffd6d8d21c765a
quintero-coyun21 <118376512+quintero-coyun21@users.noreply.github.com>
2024-05-13 20:15:54 +0200

    nada
    nada

--- Java/src/Primero_DAM/Practica5/Main.java
+++ Java/src/Primero_DAM/Practica5/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ListaNumerica lista = new ListaNumerica();
         lista.lista.add(5.5);
-
-        lista.lista.add(5.5);
-        lista.sumaElementos();
+        lista.lista.add(5.5);lista.lista.add(5.5);
         lista.promedioElementos();
     }
 }
