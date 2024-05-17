5c98a541156059c5f2c128ff0141d3e1140eb2d0
quintero-coyun21 <118376512+quintero-coyun21@users.noreply.github.com>
2024-05-17 21:02:42 +0200

    Unidad 14
    seguir.

--- Java/src/Primero_DAM/Unidad14/Main.java
+++ Java/src/Primero_DAM/Unidad14/Main.java
@@ -1,12 +1,26 @@
 package Primero_DAM.Unidad14;
 import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
     static Statement stmt = null;
     static Connection con = null;
 
     public static void main(String[] args) throws SQLException {
         establecerConexion();
-        obtenerAlumnos();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                1. Obtener alumnos
+                2. Obtener las matrículas de los alumnos
+                3. Insertar alumnos
+                4. Actualizar nota
+                """);
+        int opcion = sc.nextInt();
+        switch (opcion) {
+            case 1: obtenerAlumnos();
+            case 2: obtenerMatriculas();
+            case 3: insertarAlumnos();
+        }
         System.out.println();
         obtenerAsignaturas();
     }
@@ -40,5 +54,33 @@ public class Main {
                     \nCurso de la Asignatura:""" + " " + rs.getString("curso")
             );
         }
+    }
+
+    public static void obtenerMatriculas() throws SQLException {
+        System.out.println("Introduce el id del alumno:");
+        int idAlumnoP = new Scanner(System.in).nextInt();
+        String sql = "select alumno.idAlumno, alumno.nombre, asignatura.idAsignatura, asignatura.nombre from alumno join asignatura where alumno.idAlumno = " + idAlumnoP;
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println("""
+                    \nNombre alumno:"""+ " " + rs.getString("nombre") + """
+                    \nNombre asignatura:"""+" " + rs.getString("nombre") + """
+                    """);
+        }
+        // mostrar nombre alumno y asignatura y la nota media
+    }
+
+    public static void insertarAlumnos() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        String nombre,apellido1,apellido2;
+        System.out.println("Introduce el nombre del alumno:");
+        nombre = sc.nextLine();
+        System.out.println("Introduce el primer apellido del alumno:");
+        apellido1 = sc.nextLine();
+        System.out.println("Introduce el segundo apellido del alumno:");
+        apellido2 = sc.nextLine();
+
+        String sql = "insert into alumno(nombre,apellido1,apellido2) " + "values('" + nombre + "','" + apellido1 + "','" + apellido2 + "')";
+        stmt.executeUpdate(sql);
     }
 }
